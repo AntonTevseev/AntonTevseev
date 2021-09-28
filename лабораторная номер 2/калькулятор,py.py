e0da9f05b260395661abd4e2668825bf7c52938c
AntonTevseev <91051811+AntonTevseev@users.noreply.github.com>
2021-09-28 15:53:00 +0300

    Create калькулятор,py.py

---
+++ лабораторная номер 2/калькулятор,py.py
@@ -0,0 +1,11 @@
+what = input("+,-")
+
+a = int(input("первое число"))
+    
+b = int(input("второе число"))
+    
+if what == "+" : c = a + b
+if what == "-" : c = a - b
+
+print (c)
+print ("спасибо")
